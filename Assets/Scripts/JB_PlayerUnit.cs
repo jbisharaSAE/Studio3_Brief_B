f1f217a4277ec3b994f2d8634f9e76af1e57c562
James <j.bishara@hotmail.com>
2019-11-01 21:55:35 +1100

    Added horizontal movement for players

--- Assets/Scripts/JB_PlayerUnit.cs
+++ Assets/Scripts/JB_PlayerUnit.cs
@@ -4,16 +4,33 @@ using UnityEngine;
 using UnityEngine.Networking;
 
 
-
 public class JB_PlayerUnit : NetworkBehaviour
 {
     public HeroType heroType;
 
-    
+    private Rigidbody2D rb;
+
+    private float directionX;
+    public float moveSpeed = 5f;
+
+    public override void OnStartAuthority()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
     // Update is called once per frame
     void Update()
     {
         if(!this.hasAuthority) { return; }
+
+        directionX = Input.GetAxisRaw("Horizontal") * moveSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        if (!this.hasAuthority) { return; }
+
+        rb.velocity = new Vector2(directionX, rb.velocity.y);
     }
 
 }
