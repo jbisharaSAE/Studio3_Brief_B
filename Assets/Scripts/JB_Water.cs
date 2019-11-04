a5d1d1c8cf9a3db1f01419bb16d6ce28fdff38eb
James <j.bishara@hotmail.com>
2019-11-04 15:43:51 +1100

    Created water  - bob sinks / tot floats

---
+++ Assets/Scripts/JB_Water.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JB_Water : MonoBehaviour
+{
+  
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            if(collision.gameObject.GetComponent<JB_PlayerUnit>().heroType == HeroType.Bob)
+            {
+                // if bob hits the water, disable collider
+                Physics2D.IgnoreCollision(collision.gameObject.GetComponent<BoxCollider2D>(), this.GetComponent<BoxCollider2D>());
+            }
+        }
+    }
+}
