6039377a410e6d2ad1181e77a69e1fef0972f602
James <j.bishara@hotmail.com>
2019-11-07 19:53:04 +1100

    Imported some sprite images for groceries, created layout for list

---
+++ Assets/Scripts/JB_ToggleButton.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JB_ToggleButton : MonoBehaviour
+{
+    private bool toggle = false;
+
+    public void ToggleGameObject(GameObject obj)
+    {
+        toggle = !toggle;
+
+        obj.SetActive(toggle);
+    }
+}
