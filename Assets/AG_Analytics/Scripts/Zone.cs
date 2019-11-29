2c77e3fd46e7ab976304606e214a7e45d3c0e4b5
Cameron Shepherd <1014133@student.sae.edu.au>
2019-11-29 15:40:25 +1100

    Auto stash before merge of "master" and "origin/master"

--- Assets/AG_Analytics/Scripts/Zone.cs
+++ Assets/AG_Analytics/Scripts/Zone.cs
@@ -8,40 +8,40 @@ public class Zone : MonoBehaviour
     // Note: This does not yet track which game the events are being called from.
     //       If you have multiple games running, you won't be able to tell which game called each event.
 
-    [SerializeField] private float timer = 0f;
-    [SerializeField] private bool countingUp = false;
+    //[SerializeField] private float timer = 0f;
+    //[SerializeField] private bool countingUp = false;
+    
+    //private void Update()
+    //{
+    //    if (countingUp == true)
+    //    {
+    //        timer += Time.deltaTime;
+    //    }
+    //}
 
-    private void Update()
-    {
-        if (countingUp == true)
-        {
-            timer += Time.deltaTime;
-        }
-    }
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.TryGetComponent(out Player otherZone))
+    //    {
+    //        countingUp = true;
+    //        string newMessage = "[" + other.gameObject.name + "] entered [" + gameObject.name + "] with timer at [" + timer + "] seconds.";
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.TryGetComponent(out Player otherZone))
-        {
-            countingUp = true;
-            string newMessage = "[" + other.gameObject.name + "] entered [" + gameObject.name + "] with timer at [" + timer + "] seconds.";
+    //        // enteredZone [Red Zone] (as example)
+    //        Analytics.CustomEvent(newMessage);
+    //        Debug.Log(newMessage);
+    //    }
+    //}
 
-            // enteredZone [Red Zone] (as example)
-            Analytics.CustomEvent(newMessage);
-            Debug.Log(newMessage);
-        }
-    }
+    //private void OnTriggerExit(Collider other)
+    //{
+    //    if (other.TryGetComponent(out Player otherZone))
+    //    {
+    //        countingUp = false;
+    //        string newMessage = "[" + other.gameObject.name + "] exited [" + gameObject.name + "]. Total time spent in [" + gameObject.name + "] was [" + timer + "] seconds.";
 
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.TryGetComponent(out Player otherZone))
-        {
-            countingUp = false;
-            string newMessage = "[" + other.gameObject.name + "] exited [" + gameObject.name + "]. Total time spent in [" + gameObject.name + "] was [" + timer + "] seconds.";
-
-            // enteredZone [Red Zone] (as example)
-            Analytics.CustomEvent(newMessage);
-            Debug.Log(newMessage);
-        }
-    }
+    //        // enteredZone [Red Zone] (as example)
+    //        Analytics.CustomEvent(newMessage);
+    //        Debug.Log(newMessage);
+    //   }
+    //}
 }
