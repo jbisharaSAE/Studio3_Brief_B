abe15f556d2e4685c155e1a20c065ae8a829cb2a
Cameron Shepherd <1014133@student.sae.edu.au>
2019-11-29 14:35:01 +1100

    imported AG_Analytics_Example_Scene into project

---
+++ Assets/AG_Analytics/Scripts/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField] private Vector3 offset;
+
+    void LateUpdate()
+    {
+        if (target)
+        {
+            transform.position = target.position + offset;
+            transform.LookAt(target);
+        }
+    }
+}
---
+++ Assets/AG_Analytics/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float speed = 10f;
+    [SerializeField] private float jumpPower = 500;
+    [SerializeField] Rigidbody rbody;
+
+    private void Start()
+    {
+        if (TryGetComponent<Rigidbody>(out Rigidbody rbComponent))
+        {
+            rbody = rbComponent;
+        }
+    }
+
+    void Update()
+    {
+        transform.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")) * speed * Time.deltaTime;
+
+        if (Input.GetKeyDown("space"))
+        {
+            rbody.AddForce(Vector3.up * jumpPower);
+        }
+    }
+}
